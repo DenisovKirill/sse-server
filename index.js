bf038f3ba47b6bbf89da05a182515210b2979298
Kirill Denisov <kirill.denisov.khar@gmail.com>
2025-04-17 19:14:13 +0300

    SSE-server

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+
+let clients = [];
+
+app.get('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  res.write(`event: connected\ndata: connected\n\n`);
+  clients.push(res);
+  sendSessionCount();
+
+  const keepAlive = setInterval(() => {
+    res.write(':\n\n');
+  }, 30000);
+
+  req.on('close', () => {
+    clearInterval(keepAlive);
+    clients = clients.filter((c) => c !== res);
+    sendSessionCount();
+  });
+});
+
+function sendSessionCount() {
+  const data = `data: ${clients.length}\n\n`;
+  clients.forEach((res) => {
+    try {
+      res.write(`event: sessionCount\n${data}`);
+    } catch (err) {
+      console.warn('[SSE] Failed to write to client:', err);
+    }
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`SSE server running at http://localhost:${PORT}`);
+});
